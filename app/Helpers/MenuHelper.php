9e4d024a43c24efca338d7112527522952a3fba7
Dery Supriady <deerysupriady@gmail.com>
2025-09-05 11:45:35 +0700

    feat(MenuHelper): add holiday management menu item
    Add new menu item for holiday management under system management section, accessible to users with 'manage-users' permission

--- app/Helpers/MenuHelper.php
+++ app/Helpers/MenuHelper.php
@@ -61,7 +61,8 @@ class MenuHelper
                         ['title' => 'Role & Permission', 'route' => 'role-permission-management', 'permission' => 'manage-roles'],
                         ['title' => 'Pakta Integritas', 'route' => 'pakta-integritas-management', 'permission' => 'manage-users'],
                         ['title' => 'Magic Link & QR', 'route' => 'magic-link-management', 'permission' => 'manage-siswa'],
-                        ['title' => 'Data Perpustakaan', 'route' => 'perpustakaan-management', 'permission' => 'manage-perpustakaan']
+                        ['title' => 'Data Perpustakaan', 'route' => 'perpustakaan-management', 'permission' => 'manage-perpustakaan'],
+                        ['title' => 'Manajemen Hari Libur', 'route' => 'hari-libur-management', 'permission' => 'manage-users']
                     ]
                 ]
             ];
